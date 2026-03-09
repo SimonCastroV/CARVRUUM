814f3c8b83d75fa05d6890157c678679619716c7
Leidy <lgallov@eafit.edu.co>
2026-03-09 11:53:53 -0500

    RF Editar la información del vehículo

--- cars/urls.py
+++ cars/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path("", views.cars_list, name="cars_list"),
     path("new/", views.car_create, name="car_create"),
     path("<int:car_id>/", views.car_detail, name="car_detail"),
+    path("<int:car_id>/edit/", views.car_edit, name="car_edit"),
     path("<int:car_id>/favorite/", views.toggle_favorite, name="toggle_favorite"),
 ]
